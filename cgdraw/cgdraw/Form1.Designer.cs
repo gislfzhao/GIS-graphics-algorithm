7884e40f4a2d94351e24ea5ea53b8d5ad106f565
Zhao <32775389+linfengzhao@users.noreply.github.com>
2017-11-13 07:35:36 -0600

    circle ,ellipse ,line ,polygon
    I am learning GIS graphics algorithm course .And I finished this project in 16th,October.

---
+++ cgdraw/cgdraw/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace cgdraw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStripFrm = new System.Windows.Forms.MenuStrip();
+            this.画线算法ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.DotLineMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.DDAMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.BresenhamMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.画圆算法ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MidpotCircleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bresenhamCircleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.画椭圆算法ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MidPotEllipseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dDA椭圆算法ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ColorSetMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CloseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ClearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PolygonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Seed_FillingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.line_fillingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.draw_polygonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripFrm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStripFrm
+            // 
+            this.menuStripFrm.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.menuStripFrm.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.画线算法ToolStripMenuItem,
+            this.画圆算法ToolStripMenuItem,
+            this.画椭圆算法ToolStripMenuItem,
+            this.PolygonToolStripMenuItem,
+            this.设置ToolStripMenuItem,
+            this.文件ToolStripMenuItem});
+            this.menuStripFrm.Location = new System.Drawing.Point(0, 0);
+            this.menuStripFrm.Name = "menuStripFrm";
+            this.menuStripFrm.Size = new System.Drawing.Size(635, 25);
+            this.menuStripFrm.TabIndex = 0;
+            this.menuStripFrm.Text = "menuStripFrm";
+            // 
+            // 画线算法ToolStripMenuItem
+            // 
+            this.画线算法ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.DotLineMenuItem,
+            this.DDAMenuItem,
+            this.BresenhamMenuItem});
+            this.画线算法ToolStripMenuItem.Name = "画线算法ToolStripMenuItem";
+            this.画线算法ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.画线算法ToolStripMenuItem.Text = "画线算法";
+            this.画线算法ToolStripMenuItem.Click += new System.EventHandler(this.画线算法ToolStripMenuItem_Click);
+            // 
+            // DotLineMenuItem
+            // 
+            this.DotLineMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.DotLineMenuItem.Name = "DotLineMenuItem";
+            this.DotLineMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.DotLineMenuItem.Text = "逐点比较法";
+            this.DotLineMenuItem.Click += new System.EventHandler(this.DotLineMenuItem_Click);
+            // 
+            // DDAMenuItem
+            // 
+            this.DDAMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.DDAMenuItem.Name = "DDAMenuItem";
+            this.DDAMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.DDAMenuItem.Text = "DDA数字微分";
+            this.DDAMenuItem.Click += new System.EventHandler(this.DDAMenuItem_Click);
+            // 
+            // BresenhamMenuItem
+            // 
+            this.BresenhamMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BresenhamMenuItem.Name = "BresenhamMenuItem";
+            this.BresenhamMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.BresenhamMenuItem.Text = "Bresenham算法";
+            this.BresenhamMenuItem.Click += new System.EventHandler(this.BresenhamMenuItem_Click);
+            // 
+            // 画圆算法ToolStripMenuItem
+            // 
+            this.画圆算法ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MidpotCircleToolStripMenuItem,
+            this.bresenhamCircleToolStripMenuItem});
+            this.画圆算法ToolStripMenuItem.Name = "画圆算法ToolStripMenuItem";
+            this.画圆算法ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.画圆算法ToolStripMenuItem.Text = "画圆算法";
+            this.画圆算法ToolStripMenuItem.Click += new System.EventHandler(this.画圆算法ToolStripMenuItem_Click);
+            // 
+            // MidpotCircleToolStripMenuItem
+            // 
+            this.MidpotCircleToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.MidpotCircleToolStripMenuItem.Name = "MidpotCircleToolStripMenuItem";
+            this.MidpotCircleToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.MidpotCircleToolStripMenuItem.Text = "中点圆算法";
+            this.MidpotCircleToolStripMenuItem.Click += new System.EventHandler(this.MidpotCircleToolStripMenuItem_Click);
+            // 
+            // bresenhamCircleToolStripMenuItem
+            // 
+            this.bresenhamCircleToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bresenhamCircleToolStripMenuItem.Name = "bresenhamCircleToolStripMenuItem";
+            this.bresenhamCircleToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.bresenhamCircleToolStripMenuItem.Text = "Bresenham算法";
+            this.bresenhamCircleToolStripMenuItem.Click += new System.EventHandler(this.bresenhamCircleToolStripMenuItem_Click);
+            // 
+            // 画椭圆算法ToolStripMenuItem
+            // 
+            this.画椭圆算法ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MidPotEllipseToolStripMenuItem,
+            this.dDA椭圆算法ToolStripMenuItem});
+            this.画椭圆算法ToolStripMenuItem.Name = "画椭圆算法ToolStripMenuItem";
+            this.画椭圆算法ToolStripMenuItem.Size = new System.Drawing.Size(80, 21);
+            this.画椭圆算法ToolStripMenuItem.Text = "画椭圆算法";
+            this.画椭圆算法ToolStripMenuItem.Click += new System.EventHandler(this.画椭圆算法ToolStripMenuItem_Click);
+            // 
+            // MidPotEllipseToolStripMenuItem
+            // 
+            this.MidPotEllipseToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.MidPotEllipseToolStripMenuItem.Name = "MidPotEllipseToolStripMenuItem";
+            this.MidPotEllipseToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.MidPotEllipseToolStripMenuItem.Text = "中点椭圆算法";
+            this.MidPotEllipseToolStripMenuItem.Click += new System.EventHandler(this.MidPotEllipseToolStripMenuItem_Click);
+            // 
+            // dDA椭圆算法ToolStripMenuItem
+            // 
+            this.dDA椭圆算法ToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dDA椭圆算法ToolStripMenuItem.Name = "dDA椭圆算法ToolStripMenuItem";
+            this.dDA椭圆算法ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.dDA椭圆算法ToolStripMenuItem.Text = "DDA椭圆算法";
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ColorSetMenuItem});
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.设置ToolStripMenuItem.Text = "设置";
+            // 
+            // ColorSetMenuItem
+            // 
+            this.ColorSetMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ColorSetMenuItem.Name = "ColorSetMenuItem";
+            this.ColorSetMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ColorSetMenuItem.Text = "颜色";
+            this.ColorSetMenuItem.Click += new System.EventHandler(this.ColorSetMenuItem_Click);
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CloseToolStripMenuItem,
+            this.ClearToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // CloseToolStripMenuItem
+            // 
+            this.CloseToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem";
+            this.CloseToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.CloseToolStripMenuItem.Text = "退出";
+            this.CloseToolStripMenuItem.Click += new System.EventHandler(this.CloseToolStripMenuItem_Click);
+            // 
+            // ClearToolStripMenuItem
+            // 
+            this.ClearToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem";
+            this.ClearToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ClearToolStripMenuItem.Text = "清除";
+            this.ClearToolStripMenuItem.Click += new System.EventHandler(this.ClearToolStripMenuItem_Click);
+            // 
+            // PolygonToolStripMenuItem
+            // 
+            this.PolygonToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.draw_polygonToolStripMenuItem,
+            this.Seed_FillingToolStripMenuItem,
+            this.line_fillingToolStripMenuItem});
+            this.PolygonToolStripMenuItem.Name = "PolygonToolStripMenuItem";
+            this.PolygonToolStripMenuItem.Size = new System.Drawing.Size(56, 21);
+            this.PolygonToolStripMenuItem.Text = "多边形";
+            this.PolygonToolStripMenuItem.Click += new System.EventHandler(this.PolygonToolStripMenuItem_Click);
+            // 
+            // Seed_FillingToolStripMenuItem
+            // 
+            this.Seed_FillingToolStripMenuItem.Name = "Seed_FillingToolStripMenuItem";
+            this.Seed_FillingToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.Seed_FillingToolStripMenuItem.Text = "种子填充算法";
+            this.Seed_FillingToolStripMenuItem.Click += new System.EventHandler(this.Seed_FillingToolStripMenuItem_Click);
+            // 
+            // line_fillingToolStripMenuItem
+            // 
+            this.line_fillingToolStripMenuItem.Name = "line_fillingToolStripMenuItem";
+            this.line_fillingToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.line_fillingToolStripMenuItem.Text = "扫描线填充算法";
+            this.line_fillingToolStripMenuItem.Click += new System.EventHandler(this.line_fillingToolStripMenuItem_Click);
+            // 
+            // draw_polygonToolStripMenuItem
+            // 
+            this.draw_polygonToolStripMenuItem.Name = "draw_polygonToolStripMenuItem";
+            this.draw_polygonToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.draw_polygonToolStripMenuItem.Text = "绘制多边形";
+            this.draw_polygonToolStripMenuItem.Click += new System.EventHandler(this.draw_polygonToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 528);
+            this.Controls.Add(this.menuStripFrm);
+            this.MainMenuStrip = this.menuStripFrm;
+            this.Name = "Form1";
+            this.Text = "GIS图形算法程序";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStripFrm.ResumeLayout(false);
+            this.menuStripFrm.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripFrm;
+        private System.Windows.Forms.ToolStripMenuItem 画线算法ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem DotLineMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ColorSetMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem BresenhamMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem DDAMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CloseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 画圆算法ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MidpotCircleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bresenhamCircleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 画椭圆算法ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MidPotEllipseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dDA椭圆算法ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ClearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem PolygonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Seed_FillingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem line_fillingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem draw_polygonToolStripMenuItem;
+    }
+}
+
---
+++ cgdraw/cgdraw/Form1.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace cgdraw
+{
+    public partial class Form1 : Form
+    {
+        int mLineType = -1;
+        int mCicleType = -1;
+        int mEllipseType = -1;
+        int mPolygonType = -1;
+        polygon mypoly;
+        string str ;
+        Point pt1, pt2;
+        //int count = 0;
+        //bool controlMouseDown = false;//保证鼠标移动划线是在鼠标点击之后开始进行
+
+        private IntPtr hwnd;
+        private IntPtr hdc;
+        Color mColor = Color.Red;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DotLineMenuItem_Click(object sender, EventArgs e)
+        {
+            mLineType = 1;
+        }
+        private void DDAMenuItem_Click(object sender, EventArgs e)
+        {
+            mLineType = 2;
+        }
+
+        private void BresenhamMenuItem_Click(object sender, EventArgs e)
+        {
+            mLineType = 3;
+        }
+        private void MidpotCircleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mCicleType = 1;
+        }
+        private void bresenhamCircleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mCicleType = 2;
+        }
+
+        private void MidPotEllipseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mEllipseType = 1;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (mPolygonType == 1)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    hdc = useApi.GetDC(hwnd);
+                    Point newpoint = new Point(e.X, e.Y);
+                    mypoly.AddVertex(newpoint);
+                    if (mypoly.m_NumVertex >= 2)
+                        line.bresenhamLine(hdc, mypoly.getPoint(mypoly.m_NumVertex - 1).X, mypoly.getPoint(mypoly.m_NumVertex - 1).Y, mypoly.getPoint(mypoly.m_NumVertex).X, mypoly.getPoint(mypoly.m_NumVertex).Y, Color.Red);
+                    useApi.ReleaseDC(hwnd, hdc);
+                }
+                else if (e.Button == MouseButtons.Right)
+                {
+                    hdc = useApi.GetDC(hwnd);
+                    Point newpoint = new Point(e.X, e.Y);
+                    mypoly.AddVertex(newpoint);
+                    // if (mypoly.m_NumVertex >= 2)
+                    line.bresenhamLine(hdc, mypoly.getPoint(mypoly.m_NumVertex - 1).X, mypoly.getPoint(mypoly.m_NumVertex - 1).Y, mypoly.getPoint(mypoly.m_NumVertex).X, mypoly.getPoint(mypoly.m_NumVertex).Y, Color.Red);
+                    line.bresenhamLine(hdc, mypoly.getPoint(1).X, mypoly.getPoint(1).Y, mypoly.getPoint(mypoly.m_NumVertex).X, mypoly.getPoint(mypoly.m_NumVertex).Y, Color.Red);
+
+                    useApi.ReleaseDC(hwnd, hdc);
+
+                }
+            }
+            if (mPolygonType == 2)
+            {
+                hdc = useApi.GetDC(hwnd);
+                Point newseed = new Point(e.X, e.Y);
+                string str = mypoly.seedfill(hdc, newseed, Color.Blue, Color.Red);
+                useApi.ReleaseDC(hwnd, hdc);
+                MessageBox.Show(str);
+
+
+            }
+            #region 绘图
+            pt1.X = e.X;
+            pt1.Y = e.Y;
+            #endregion
+            //controlMouseDown = true;
+
+            #region 只需要点击两点就生成直线
+            //if (mLineType == 1||mLineType==2)
+            //{
+            //    if (count == 0)
+            //    {
+            //        pt1.X = e.X;
+            //        pt1.Y = e.Y;
+            //    }
+            //    if (count == 1)
+            //    {
+            //        pt2.X = e.X;
+            //        pt2.Y = e.Y;
+
+            //    }
+
+            //}
+            #endregion
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            #region 鼠标移动
+            //if (mLineType != -1)
+            //{
+            //    //保证鼠标移动时出轨迹
+            //    if (controlMouseDown)
+            //    {
+            //        if (mLineType != -1)
+            //        {
+            //            pt2.X = e.X;
+            //            pt2.Y = e.Y;
+            //            sPaint();
+            //            pt1.X = pt2.X;
+            //            pt1.Y = pt2.Y;
+            //        }
+            //    }
+            //}
+            #endregion
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            #region 绘图
+            if (mPolygonType != -1)
+            {
+
+            }
+            if (mLineType != -1 || mCicleType != -1 || mEllipseType != -1)
+            {
+                pt2.X = e.X;
+                pt2.Y = e.Y;
+            }
+            if (mLineType != -1)
+            {
+                sPaint();
+                //controlMouseDown = false;
+            }
+            if (mCicleType != -1)
+            {
+                line.ddaLine(hdc, pt1.X, pt1.Y, pt2.X, pt2.Y, mColor);
+                CirclePaint();
+            }
+            if (mEllipseType != -1)
+            {
+                EllipsePaint();
+            }
+            if (str != null)
+            {
+                MessageBox.Show(str);
+            }
+            #endregion
+
+            #region 只需要点击两点就生成直线
+            //if (mLineType == 1 || mLineType == 2)
+            //{
+            //    if (count == 1)
+            //    {
+            //        sPaint();
+            //        count++;
+            //    }
+            //    if (count == 0)
+            //        count++;
+            //    if (count == 2)
+            //        count = 0;
+            //}
+            #endregion
+        }
+
+        public void sPaint()
+        {
+            hdc = useApi.GetDC(hwnd);
+            if (mLineType == 1)
+            {
+                str = line.potLine(hdc, pt1.X, pt1.Y, pt2.X, pt2.Y, mColor);
+                
+            }
+            if (mLineType == 2)
+            {
+                str = line.ddaLine(hdc, pt1.X, pt1.Y, pt2.X, pt2.Y, mColor);
+            }
+            if (mLineType == 3)
+            {
+                str =line.bresenhamLine(hdc, pt1.X, pt1.Y, pt2.X, pt2.Y, mColor);
+
+            }
+            useApi.ReleaseDC(hwnd, hdc);
+
+        }
+        public void CirclePaint()
+        {
+            hdc = useApi.GetDC(hwnd);
+            if (mCicleType == 1)
+            {
+                str=circle.MidpotCircle(hdc, pt1.X, pt1.Y, pt2.X, pt2.Y, mColor);
+            }
+            if (mCicleType == 2)
+            {
+                str=circle.BresenhamPotCircle(hdc, pt1.X, pt1.Y, pt2.X, pt2.Y, mColor);
+            }
+            useApi.ReleaseDC(hwnd, hdc);
+        }
+        public void EllipsePaint()
+        {
+            hdc = useApi.GetDC(hwnd);
+            line.ddaLine(hdc, pt1.X, pt1.Y, pt2.X, pt1.Y, mColor);
+            line.ddaLine(hdc, pt1.X, pt1.Y, pt1.X, pt2.Y, mColor);
+            line.ddaLine(hdc, pt1.X, pt2.Y, pt2.X, pt2.Y, mColor);
+            line.ddaLine(hdc, pt2.X, pt1.Y, pt2.X, pt2.Y, mColor);
+            if (mEllipseType == 1)
+            {
+                str=ellipse.MidpotEllipse(hdc, pt1.X, pt1.Y, pt2.X, pt2.Y, mColor);
+            }
+            useApi.ReleaseDC(hwnd, hdc);
+        }
+        public void fillingPaint()
+        {
+            hdc = useApi.GetDC(hwnd);
+            if (mPolygonType == 3)
+            {
+                mypoly.linefill(hdc, Color.Pink);
+            }
+            useApi.ReleaseDC(hwnd, hdc);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hwnd = useApi.FindWindow(null, "GIS图形算法程序"); //获取窗口句柄
+            hdc = useApi.GetDC(hwnd);          //获取设备
+
+
+
+            //if (hwnd != IntPtr.Zero)
+            //{
+            //    MessageBox.Show("找到窗口");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("没有找到窗口");
+            //}
+
+        }
+
+        private void CloseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("您确定退出吗？","提示信息",MessageBoxButtons.OKCancel)==DialogResult.OK)
+            {
+                System.Environment.Exit(0);
+            }
+            
+        }
+
+        
+        private void 画线算法ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (mCicleType != -1 || mEllipseType != -1)
+            {
+                mEllipseType = -1;
+                mCicleType = -1;
+            }
+        }
+
+        private void 画圆算法ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (mLineType != -1 || mEllipseType != -1)
+            {
+                mEllipseType = -1;
+                mLineType = -1;
+            }
+        }
+
+        private void 画椭圆算法ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (mLineType != -1|| mCicleType != -1)
+            {
+                mCicleType = -1;
+                mLineType = -1;
+            }
+        }
+
+        private void ClearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Application.Restart();
+            this.Refresh();
+        }
+
+        private void PolygonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (mLineType != -1 || mEllipseType != -1 || mCicleType != -1)
+            {
+                mEllipseType = -1;
+                mLineType = -1;
+                mCicleType = -1;
+            }
+        }
+
+        private void ColorSetMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog mcolorDig = new ColorDialog();
+            if (mcolorDig.ShowDialog() == DialogResult.OK)
+                mColor = mcolorDig.Color;
+        }
+
+        private void draw_polygonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mPolygonType = 1;
+            mypoly = new polygon();
+        }
+
+        private void Seed_FillingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mPolygonType = 2;
+        }
+
+        private void line_fillingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mPolygonType = 3;
+            fillingPaint();
+        }
+        
+    }
+}
